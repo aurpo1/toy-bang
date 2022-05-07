52d0790db802e0d496d5cf114a1fce579bb352e0
aurpo1 <aurpo1@naver.com>
2022-05-07 23:32:01 +0900

    feat: coin box scroll
    swiper는 해결 하지 못 한 상태

--- app.js
+++ app.js
@@ -3,26 +3,10 @@
 
   console.log('ok');
 
-  const swiper = new Swiper('.swiper', {
-    // // Optional parameters
-    // direction: 'vertical',
-    // loop: true,
-  
-    // // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
-  
-    // // Navigation arrows
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
-  
-    // // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
+  const swiper = new Swiper('.mySwiper', {
+    pagination: {
+      el: ".swiper-pagination",
     },
-  );
+   });
 
   let itemSwiper = new Swiper('.history_sliderlist', {});
