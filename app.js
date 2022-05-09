2e9aa78f5ac585b2ff066482fe28f175d3de47af
aurpo1 <aurpo1@naver.com>
2022-05-09 15:47:13 +0900

    feat: daily report chart!

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ console.log(itemSwiper);
 
 // Chart
 
-const labels = [
+const labels = [ //x축
   '02', '04', '06', '08', '10',
   '12', '14', '16', '18', '20',
   '22', '24', '26', '28', '30'
@@ -23,20 +23,58 @@ const labels = [
 const data = {
   labels: labels,
   datasets: [{
-    label: 'daily report',
+    type: 'bar',
+    label: 'daily bar report',
     backgroundColor: '#38C976',
-    data: [
+    barThickness: 4,
+    barRadius: 10, 
+    data: [ //y축 data
       65000, 54300, 45000, 48000, 67000,
       65000, 54300, 45000, 48000, 67000,
       65000, 54300, 45000, 48000, 67000
     ],
-  }]
+  }, {
+    type: 'line',
+    label: 'daily line report',
+    borderColor: '#FF5F00',
+    borderDash: [10, 5],
+    pointStyle: 'line',
+    data: [ //y축 data
+    65000, 54300, 45000, 48000, 67000,
+    65000, 54300, 45000, 48000, 67000,
+    65000, 54300, 45000, 48000, 67000
+  ],
+  }],
 };
 
 const config = {
   type: 'bar',
   data: data,
-  options: {}
+  options: {
+    responsive: false,
+    plugins: {
+      legend: {
+        display: false, //label 숨기기
+      },
+    },
+    scales: {
+      x: {
+        base: 20000,
+        grid: {
+          display: false,
+        }
+      },
+      y: {
+        min: 20000,
+        grid: {
+          borderDash: [5, 5],
+        },
+        ticks: { //단위 설정
+          stepSize: 20000,
+        }
+      }
+    },
+  }
 };
 
 const dailyChart = new Chart(
